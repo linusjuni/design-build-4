1c7b2812b9fcad2b163960270ee78917f8c6cced
linusjuni <s225224@student.dtu.dk>
2024-06-07 13:47:03 +0200

    weird shit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return the React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  res.sendFile(path.join(path.resolve(__dirname, 'src', 'client', 'build', 'index.html')));
 });
 
 app.listen(PORT, () => {
