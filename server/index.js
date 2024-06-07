578ce4a30b54b865b228019b917b582125aab7ec
linusjuni <s225224@student.dtu.dk>
2024-06-07 12:12:49 +0200

    now?

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.get('/api', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.js'));
 });
 
 app.listen(PORT, () => {
