204d15075e1d284f174f5df081e287e3f3d543d3
linusjuni <s225224@student.dtu.dk>
2024-06-07 10:30:54 +0200

    added build index for node app

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/api', (req, res) => {
     res.json({ message: "Hello from server!" });
